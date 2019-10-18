e89971b4926dd628b36073cf2f41e607eb906e13
JoseCurti <josecurti.jc@gmail.com>
2019-10-17 23:39:15 -0300

    Acomodando Recien

--- BackEnd/Models/ApplicationDbContext.cs
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -20,15 +20,15 @@ namespace BackEnd.Models
 
         public DbSet<intereses> Intereses { get; set; }
 
-        public DbSet<InteresesUsuarios> InteresesUsuarios { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        //public DbSet<InteresesUsuarios> InteresesUsuarios { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<InteresesUsuarios>()
-                    .HasKey(o => new { o.Usuario.Id, o.InteresesFK });
+        //    modelBuilder.Entity<InteresesUsuarios>()
+        //            .HasKey(o => new { o.Usuario.Id, o.InteresesFK });
 
-        }
+        //}
     }
 }
 
